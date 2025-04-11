d23b190c7d83c4655149e5007dd4c519373b4d96
Adam Mendoza <adammendoza27@gmail.com>
2025-04-11 00:14:39 -0500

    funnycodestuff

--- Assets/Grid.cs
+++ Assets/Grid.cs
@@ -6,7 +6,7 @@ public class Grid : MonoBehaviour
 {
     public Orb OrbPrefab;
     public Transform GridLayout;
-
+    public List<Orb> OrbElements = new();
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +14,31 @@ public class Grid : MonoBehaviour
         {
             Orb newOrb = Instantiate(OrbPrefab, GridLayout);
             newOrb.Element = (Orb.ElementType)Random.Range(0, 6);
+            OrbElements.Add(newOrb);
         }
+        checkMatch();
     }
 
+    void checkMatch()
+    {
+        for (int i = 0; i < OrbElements.Count-2; i++) { 
+        
+            if (OrbElements[i].Element == OrbElements[i + 1].Element && OrbElements[i].Element == OrbElements[i + 2].Element)
+            {
+                Destroy(OrbElements[i].gameObject);
+                Destroy(OrbElements[i + 1].gameObject);
+                Destroy(OrbElements[i + 2].gameObject);
+            }
+        }
+        for (int i = 0; i < OrbElements.Count - 16; i++) {
+            if (OrbElements[i].Element == OrbElements[i + 8].Element && OrbElements[i].Element == OrbElements[i + 16].Element)
+            {
+                Destroy(OrbElements[i].gameObject);
+                Destroy(OrbElements[i + 8].gameObject);
+                Destroy(OrbElements[i + 16].gameObject);
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
