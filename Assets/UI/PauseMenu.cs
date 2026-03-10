b8ffbc0c9aca8db12c3d6772d42032faae3e1b53
Xelalego <alexpbenton@gmail.com>
2026-03-10 11:59:59 -0700

    Pause Menu Polish
    Added color to pause menu buttons and added functionality for Escape key, Resume, and Quit buttons

--- Assets/UI/PauseMenu.cs
+++ Assets/UI/PauseMenu.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
+    public Transform Menu;
+    public static bool Paused = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Unpause();
+    }
+
+    public void TogglePause()
+    {
+        if (Paused) Unpause();
+        else Pause();
+    }
+
+    public void Unpause()
+    {
+        Paused = false;
+        Menu.gameObject.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        Paused = true;
+        Menu.gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
     }
 }
