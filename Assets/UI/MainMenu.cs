de8bd80261dc4197c7e6f5c48f732c1539d561d0
Adam Mendoza <adammendoza27@gmail.com>
2026-03-16 20:37:39 -0500

    More Main Menu UI Tweaks and code
    added working Main Menu code

--- Assets/UI/MainMenu.cs
+++ Assets/UI/MainMenu.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Transform Settings, Compenduim;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Compenduim.gameObject.activeSelf == true)
+            {
+                Compenduim.gameObject.SetActive(false);
+            }
+            else if (Settings.gameObject.activeSelf == true)
+            {
+                Settings.gameObject.SetActive(false);
+            }
+            
+        }
+    }
+
+    //create a new game
+    public void NewGame()
+    {
+        //Menu.gameObject.SetActive(false);
+    }
+    //load a current, not finished game
+    public void ContinueGame()
+    {
+
+    }
+    //Settings
+    public void GameSettings()
+    {
+        Settings.gameObject.SetActive(true);
+    }
+    //Book of Lore
+    public void GameCompedium()
+    {
+        Compenduim.gameObject.SetActive(true);
+    }
+    //you know what this does, hopefully
+    public void QuitGame()
+    {
+        //print("QUIT");
+        Application.Quit();
     }
 }
