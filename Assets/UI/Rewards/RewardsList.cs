8ee6bd8aee2708d1c196ba0a922ffb4e319be5fe
Xelalego <alexpbenton@gmail.com>
2026-03-21 10:07:41 -0700

    Reward Screen

---
+++ Assets/UI/Rewards/RewardsList.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardsList : MonoBehaviour
+{
+    public RewardUI RewardPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
