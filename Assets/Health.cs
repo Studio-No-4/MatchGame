a8aebf5db466faa79e5a619c2f38f6dbd2297ca6
Xelalego <alexpbenton@gmail.com>
2026-03-21 16:51:30 -0700

    Win/Lose scene progression

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
     public int HP = 10;
     public int MaxHP = 10;
+
+    public UnityEvent OnDeath;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +28,7 @@ public class Health : MonoBehaviour
 
     public virtual void Die()
     {
+        OnDeath.Invoke();
         Destroy(gameObject);
     }
 
