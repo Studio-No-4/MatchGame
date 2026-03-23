60a47ae678b661564844dd65b31c830663014174
Xelalego <alexpbenton@gmail.com>
2026-03-23 16:55:59 -0700

    Character Select Spell Details

--- Assets/UI/CharacterSelectScreen.cs
+++ Assets/UI/CharacterSelectScreen.cs
@@ -9,6 +9,8 @@ public class CharacterSelectScreen : MonoBehaviour
     public CharacterData DefaultCharacter;
     public TMP_Text CharacterName;
     public TMP_Text CharacterDescription;
+    public SpellWidget SpellWidget;
+    public TMP_Text SpellDescription;
     public Image Image;
 
     // Start is called before the first frame update
@@ -23,6 +25,8 @@ public class CharacterSelectScreen : MonoBehaviour
         CharacterName.text = character.name;
         CharacterDescription.text = character.Description;
         Image.sprite = character.Image;
+        SpellWidget.SetSpell(character.StartingSpells[0]);
+        SpellDescription.text = character.StartingSpells[0].Description;
         GameManager.PlayerSpells = new List<SpellData>(character.StartingSpells);
     }
 
